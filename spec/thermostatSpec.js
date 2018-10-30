8a1d7acbbf0c3d192d29595519613df790447fc8
Edyta Bednarz <edy.b.bednarz@gmail.com>
2018-10-30 15:48:18 +0000

    Creted up and down temp

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -8,7 +8,22 @@ describe('Thermostat', function() {
     thermostat = new Thermostat();
   });
 
-  it('starts at 20 degrees', function() {
+  it('starts at 20 degrees', function(){
     expect(thermostat.temperature).toEqual(20);
   });
+
+  it('starts at 20 degrees', function(){
+    expect(thermostat.newTemperature()).toEqual(20);
+  });
+
+  it('can increase the temperature', function(){
+    thermostat.increaseTempBy();
+    expect(thermostat.newTemperature()).toEqual(25);
+  });
+
+  it('can decrease the temperature', function(){
+    thermostat.decreaseTempBy();
+    expect(thermostat.newTemperature()).toEqual(15);
+  });
+
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,5 +1,17 @@
+'use strict';
 
 var Thermostat =  function() {
   this.temperature = 20;
+};
 
+Thermostat.prototype.newTemperature = function() {
+  return this.temperature;
+};
+
+Thermostat.prototype.increaseTempBy = function(){
+  this.temperature += 5;
+};
+
+Thermostat.prototype.decreaseTempBy = function(){
+  this.temperature -= 5
 };
