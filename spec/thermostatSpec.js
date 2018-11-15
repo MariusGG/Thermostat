3286e0ba819b21a981330fdf2989617247fddd10
mariusgg <marius.g.guei@gmail.com>
2018-11-15 15:26:30 +0000

    added reset test

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -21,32 +21,37 @@ describe("Thermostat", function(){
 
   it("has maximum temperature of 25", function(){
     for (var i = 0; i < 6; i++) {
-    thermostat.increase();
-  }
-  expect(thermostat.getTemperature()).toEqual(25);
-});
+      thermostat.increase();
+    }
+    expect(thermostat.getTemperature()).toEqual(25);
+  });
 
-it("has mimimum temperature of 10", function(){
-  for (var i = 0; i < 6; i++) {
-  thermostat.increase();
-}
-expect(thermostat.getTemperature()).toEqual(25);
-});
+  it("has mimimum temperature of 10", function(){
+    for (var i = 0; i < 6; i++) {
+      thermostat.increase();
+    }
+    expect(thermostat.getTemperature()).toEqual(25);
+  });
 
-it('has a minimum of 10 degrees', function() {
-  for (var i = 0; i < 11; i++) {
-    thermostat.decrease();
-  }
-  expect(thermostat.getTemperature()).toEqual(10);
-});
+  it('has a minimum of 10 degrees', function() {
+    for (var i = 0; i < 11; i++) {
+      thermostat.decrease();
+    }
+    expect(thermostat.getTemperature()).toEqual(10);
+  });
 
-it('has power saving mode on by default', function() {
-  expect(thermostat.PowerSavingModeOn()).toBe(true);
-});
+  it('has power saving mode on by default', function() {
+    expect(thermostat.PowerSavingModeOn()).toBe(true);
+  });
 
-it('can switch power save mode off', function() {
+  it('can switch power save mode off', function() {
     thermostat.switchPowerSavingModeOff();
     expect(thermostat.PowerSavingModeOn()).toBe(false);
+  });
+
+  it('can reset temperature to 20', function() {
+    thermostat.reset()
+    expect(thermostat.getTemperature()).toEqual(20);
   });
 
 
